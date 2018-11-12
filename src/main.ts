353478de72bc182c693b99ea40eaeb2e7d377f50
blagoj <blagojd1@gmail.com>
2018-11-12 15:08:23 +0100

    ELECTRON VERSION UPDATE
    Electron version update to 3.0.2 and Electron Builder version update to 20.28.1

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from 'environments';
 
+// Remove the security warnings about Content policy - not imposing security issues for our project anyways.
+window['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
+
 if (environment.production) {
   enableProdMode();
 }
